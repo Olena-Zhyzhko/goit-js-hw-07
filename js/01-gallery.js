dbe9b6e5aed31641035c61b4658d1dc7fce7d9d4
Olena-Zhyzhko <asil198609@gmail.com>
2022-07-13 22:31:52 +0300

    Do samethin

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,46 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+const galleryItem = createGalleryItems(galleryItems);
+
+gallery.insertAdjacentHTML('beforeend', galleryItem);
+
+gallery.addEventListener('click', onConteinerClick)
+
+
+function createGalleryItems(photos) {
+    return photos.map(({ preview, original, description }) => {
+        return `
+    <a class="gallery__link" href="${original}">
+        <img class="gallery__image"
+            src="${preview}"
+            alt="${description}"
+        >
+    </a>
+    `}).join('');
+};
+
+function onConteinerClick(event) {
+    const isImageElement = event.target.classList.contains('gallery__image');
+
+    if (!isImageElement) {
+        return;
+    }
+
+    // const targetImage = event.target.getAttribute("src");
+    // console.log(targetImage);
+
+    const instance = basicLightbox.create(`<img src="${event.target.dataset.source}">`)
+
+    instance.show()
+    // const isLinkImageElement = event.target.closest('.gallery__link');
+    // const largeResolutionLink = isLinkImageElement.
+
+    // console.log(isLinkImageElement);
+}
+
+
+
+// console.log()
